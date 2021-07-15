95eee80da6b727192ba511215723ca84edf1de27
Victor Lupascu <sinajeen@gmail.com>
2021-07-15 16:47:55 -0500

    add zookeeper tests, routes and functionality

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const animalRoutes = require('./animalRoutes');
 
 router.use(animalRoutes);
+router.use(require('./zookeeperRoutes'));
 
 module.exports = router;
