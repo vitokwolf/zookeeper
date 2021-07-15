e722899bb58278963656e15b67c3d59868a51bd6
Victor Lupascu <sinajeen@gmail.com>
2021-07-15 13:57:17 -0500

    add new route for id filtering

--- server.js
+++ server.js
@@ -41,7 +41,13 @@ function filterByQuery(query, animalsArray) {
     }
     // return the filtered results:
     return filteredResults;
-}
+};
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
+
 // add the route
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -49,6 +55,15 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 // method to make our server listen
 app.listen(PORT, () => {
